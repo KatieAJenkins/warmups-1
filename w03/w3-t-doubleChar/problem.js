d7f7d501fd7817f7aaaafd06ea4302212e8ba1c7
KatieAJenkins <kind2karma@gmail.com>
2017-05-07 19:31:04 -0600

    added check for string

--- w03/w3-t-doubleChar/problem.js
+++ w03/w3-t-doubleChar/problem.js
@@ -17,7 +17,8 @@
 // - Do it with a RegEx! (a 2 line solution is possible)
 
 
-let string = "love";
+// let string = "Love";
+let string1 = 8;
 
 // function doubleChar(string) {
 //   let newString = '';
@@ -30,8 +31,13 @@ let string = "love";
 // }
 
 function doubleChar(string){
+  if(typeof(string) !== 'string'){
+    console.log("Ruh roh! That isn't a string!");
+    return "Ruh roh! That isn't a string!" ;
+  }
+
   let array = string.split('');
-  // console.log(array);
+  console.log(array);
   let doubledArray = [];
   let doubledString;
    array.map(function(element){
@@ -40,11 +46,12 @@ function doubleChar(string){
   });
   // console.log(doubledArray);
   doubledString = doubledArray.join('');
-  // console.log(doubledString);
-  return doubledString;
+  console.log(doubledString);
+  // return doubledString;
 }
 
-doubleChar(string);
+// doubleChar(string);
+doubleChar(string1);
 
 // module.exports = {
 //   doubleChar:doubleChar,
