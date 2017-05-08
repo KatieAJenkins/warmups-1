8fe4f293e34f24c7e60c44b38f502c14319dc7cf
KatieAJenkins <kind2karma@gmail.com>
2017-05-07 19:22:01 -0600

    solved with for loop and map hof

--- w03/w3-t-doubleChar/problem.js
+++ w03/w3-t-doubleChar/problem.js
@@ -1,6 +1,8 @@
+'use strict';
+
 // Given a string, you have to return a string in which
 // each character (case-sensitive) is repeated once.
-// if anything but a string is passed to the function, 
+// if anything but a string is passed to the function,
 // return "Ruh roh! That isn't a string!"
 
 // doubleChar("String") ==> "SSttrriinngg"
@@ -12,11 +14,36 @@
 
 // DONE?? Do it with a RegEx! (a 2 line solution is possible)
 
-function doubleChar(str) {
+let string = "love";
 
+// function doubleChar(string) {
+//   let newString = '';
+//   for(var i = 0; i < string.length; i++){
+//     console.log(newString = newString + string[i] + string[i]);
+//     // console.log(newString);
+//   }
+//   console.log(newString);
+//   return newString;
+// }
+
+function doubleChar(string){
+  let array = string.split('');
+  // console.log(array);
+  let doubledArray = [];
+  let doubledString;
+   array.map(function(element){
+    // console.log(element + element);
+    doubledArray.push(element + element);
+  });
+  // console.log(doubledArray);
+  doubledString = doubledArray.join('');
+  // console.log(doubledString);
+  return doubledString;
 }
 
-module.exports = {
-  doubleChar:doubleChar,
-  attendance:"wordy word here"
-}
+doubleChar(string);
+
+// module.exports = {
+//   doubleChar:doubleChar,
+//   attendance:"wordy word here"
+// }
