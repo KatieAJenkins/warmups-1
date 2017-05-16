c40fcfb557ddbf2547297d6637f5157c58167797
KatieAJenkins <kind2karma@gmail.com>
2017-05-15 22:37:38 -0600

    cleaned up code

--- w03/w3-r-sum-of-numbers/problem.js
+++ w03/w3-r-sum-of-numbers/problem.js
@@ -1,10 +1,11 @@
-/* Given two integers, which can be positive and negative, 
+'use strict';
+/* Given two integers, which can be positive and negative,
 find the sum of all the numbers between and including a and b,
 and return the sum. If both numbers are equal return a or b.
 
 Note! a and b are not ordered!
 
-Example: 
+Example:
 getSum(1, 0) == 1   // 1 + 0 = 1
 getSum(1, 2) == 3   // 1 + 2 = 3
 getSum(0, 1) == 1   // 0 + 1 = 1
@@ -13,14 +14,18 @@ getSum(-1, 0) == -1 // -1 + 0 = -1
 getSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
 */
 
+//if both are equal
 function getSum( a, b ) {
-
+  if ()
+  if (a === b){
+    console.log(a);
+    return a;
+  }
 } // END FUNCTION
 
+getSum(1, 0);
 
 module.exports = {
   getSum:getSum,
   attendance:"WORD UP"
 }
-
-
